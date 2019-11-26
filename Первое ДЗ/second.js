83b358057f304c246cb8e2ef32f8053cf218ccba
Grigory Cheskidov <grigory.cheskidov@antidasoftware.com>
2019-11-26 10:37:35 +0500

    fixed spacing

--- Первое ДЗ/second.js
+++ Первое ДЗ/second.js
@@ -1,30 +1,30 @@
 function tableToObj(tableId) {
-	var table = document.getElementById(tableId);
+  var table = document.getElementById(tableId);
   var rows = table.rows;
   var parsers = new Map([
-  	['int', parseInt],
+    ['int', parseInt],
     ['float', parseFloat],
     ['date', Date.parse],
   ]);
   var objList = [];
   
   var attributeNames = Array.from(rows[0].cells).map(function (el) {
-  		return {"name": el.innerText, "type": el.getAttribute("data-type")} 
+      return {"name": el.innerText, "type": el.getAttribute("data-type")} 
     }
   );
 
   for(i = 1; i < rows.length; i ++) {
-  	var obj = {};
-  	
+    var obj = {};
+    
     for(j = 0; j < rows[i].cells.length; j++){
-    	attribute = attributeNames[j];
+      attribute = attributeNames[j];
       cell = rows[i].cells[j];
       
       if (attribute.type){
-      	parser = parsers.get(attribute.type);
-    		obj[attribute.name] = parser(cell.innerText);
+        parser = parsers.get(attribute.type);
+        obj[attribute.name] = parser(cell.innerText);
       } else {
-      	obj[attribute.name] = cell.innerText;
+        obj[attribute.name] = cell.innerText;
       }
  
     }
--- Первое ДЗ/third.js
+++ Первое ДЗ/third.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = process.argv[2];
 
 fs.readFile(path, 'utf8', (err, data) => {
-	if(err) throw err;
+    if(err) throw err;
     var fixedString = data.replace(/\s{2,}/gi, ' ');
 
     fs.writeFile('converted_' + path, fixedString, (err) => {
