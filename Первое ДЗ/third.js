614ac58a0dd61c836fc88511a5e75f8320f2e683
Grigory Cheskidov <grigory.cheskidov@antidasoftware.com>
2019-11-26 10:33:16 +0500

    fixed third

--- Первое ДЗ/third.js
+++ Первое ДЗ/third.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
 const path = process.argv[2];
 
-if (path) {
-	fs.readFile(path, 'utf8', (err, data) => {
-		var fixedString = data.replace(/\s{2,}/gi, ' ');
+fs.readFile(path, 'utf8', (err, data) => {
+	if(err) throw err;
+    var fixedString = data.replace(/\s{2,}/gi, ' ');
 
-		fs.writeFile('converted_text.txt', fixedString, (err) => {
-			if (err) console.log(err);
-		});
-
-	});
-} else {
-	console.log('No path supplied!');
-};
-
+    fs.writeFile('converted_' + path, fixedString, (err) => {
+        if (err) throw err;
+    });
+});
 
